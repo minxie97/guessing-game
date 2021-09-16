66e464608181ab5f833393d35197997e3f103735
John Chavez <jrc2855@gmail.com>
2021-09-16 18:41:53 -0400

    Driver Changes

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 'use strict'
 
-let identity = prompt('Hello! What is your name?');
-while (!identity) {
-    identity = prompt('Ok but actually what is your name?')
+function identify(){
+    let identity = prompt('Hello! What is your name?');
+    while (!identity) {
+        identity = prompt('Ok but actually what is your name?')
+    }
+
+
+    // console.log('Nice to meet you ' + identity + '! Thank you for visiting my site!');
+    alert('Nice to meet you ' + identity + '! Thank you for visiting my site!');
 }
 
-// console.log('Nice to meet you ' + identity + '! Thank you for visiting my site!');
-alert('Nice to meet you ' + identity + '! Thank you for visiting my site!');
 
-let consent = prompt('I made a little quiz about myself, would you like to take it?');
-if(consent.toLowerCase() == 'yes' || consent.toLowerCase() == 'y'){
-    // console.log('Alright let\'s get started!');
-    alert('Alright let\'s get started!');
 
+
+function quiz(){
     let score = 0    
 
     let questions = ['I am originally from California.', 'My favorite nba team is the Golden State Warriors.',
@@ -80,11 +82,22 @@ if(consent.toLowerCase() == 'yes' || consent.toLowerCase() == 'y'){
     // console.log('You are all done ' + identity + '! Thanks for taking the quiz!');
     alert('You are all done ' + identity + '! Thanks for taking the quiz!');
     alert('You got ' + score + ' out of 7 points!');
+}
+
+identify();
+
+let consent = prompt('I made a little quiz about myself, would you like to take it?');
+if(consent.toLowerCase() == 'yes' || consent.toLowerCase() == 'y'){
+    // console.log('Alright let\'s get started!');
+    alert('Alright let\'s get started!');
+
+quiz();
 
 } else if(consent.toLowerCase() == 'no' || consent.toLowerCase() == 'n'){
     // console.log('I mean I don\'t blame you ' + identity + ', who would choose to do a quiz voluntarily?');
     alert('I mean I don\'t blame you ' + identity + ', who would choose to do a quiz voluntarily?');
     }
+
 
 
     
