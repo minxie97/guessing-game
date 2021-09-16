e616599c61a9a51a12a1ba81c144c3a454fe621b
Minhang Xie <minxie97@gmail.com>
2021-09-15 19:58:44 -0700

    made my code DRY

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ while (!identity) {
     identity = prompt('Ok but actually what is your name?')
 }
 
-// console.log('Nice to meet you ' + identity + '! Thank you for visiting my site!');
+console.log('Nice to meet you ' + identity + '! Thank you for visiting my site!');
 alert('Nice to meet you ' + identity + '! Thank you for visiting my site!');
 
 let consent = prompt('I made a little quiz about myself, would you like to take it?');
@@ -15,51 +15,23 @@ if(consent.toLowerCase() == 'yes' || consent.toLowerCase() == 'y'){
 
     let score = 0    
 
-    let location = prompt('I am originally from California.');
-    if(location.toLowerCase() == 'yes' || location.toLowerCase() == 'y'){
-        // console.log('That\'s right! I am originally from the Bay Area!');
-        alert('That\'s right! I am originally from the Bay Area!');
-        score++;
-    } 
-    else if(location.toLowerCase() == 'no' || location.toLowerCase() == 'n'){
-        // console.log('Nope! I\'m actually originally from the Bay Area.');
-        alert('Nope! I\'m actually originally from the Bay Area.');
-    }
-    let basketball = prompt('My favorite nba team is the Golden State Warriors.');
-    if(basketball.toLowerCase() == 'yes' || basketball.toLowerCase() == 'y'){
-        // console.log('Hell yeah, Dubnation baby');
-        alert('Hell yeah, Dubnation baby');
-        score++;
-    } else if(basketball.toLowerCase() == 'no' || basketball.toLowerCase() == 'n'){
-        // console.log('If you get to know me, you\'ll see that all I ever talk about are the Warriors.');
-        alert('If you get to know me, you\'ll see that all I ever talk about are the Warriors.');
-    }
-    let cooking = prompt('I love to cook.');
-    if(cooking.toLowerCase() == 'yes' || cooking.toLowerCase() == 'y'){
-        // console.log('Yup! It\'s one of my favorite activities! Even though I can never figure out what to make lol.');
-        alert('Yup! It\'s one of my favorite activities! Even though I can never figure out what to make lol.');
-        score++;
-    } else if(cooking.toLowerCase() == 'no' || cooking.toLowerCase() == 'n'){
-        // console.log('Let me cook something for you sometimes, you\'ll see.');
-        alert('Let me cook something for you sometimes, you\'ll see.');
-    }
-    let cat = prompt('I have a cat named Pilot Jones (Jonesy for short).')
-    if(cat.toLowerCase() == 'yes' || cat.toLowerCase() == 'y'){
-        // console.log('Yup wanna see pictures?');
-        alert('Yup wanna see pictures?');
-        score++;
-    } else if(cat.toLowerCase() == 'no' || cat.toLowerCase() == 'n'){
-        // console.log('I do so! Wanna see pictures?');
-        alert('I do so! Wanna see pictures?');
-    }
-    let band = prompt('My favorite band is Radiohead');
-    if(band.toLowerCase() == 'yes' || band.toLowerCase() == 'y'){
-        // console.log('Ding ding ding! I\'ll trade my first born child for another album.');
-        alert('Ding ding ding! I\'ll trade my first born child for another album.');
-        score++;
-    } else if(band.toLowerCase() == 'no' || band.toLowerCase() == 'n'){
-        // console.log('I mean don\'t I look depressed and pretenious to you?');
-        alert('I mean don\'t I look depressed and pretenious to you?');
+    let questions = ['I am originally from California.', 'My favorite nba team is the Golden State Warriors.',
+                    'I love to cook.', 'I have a cat named Pilot Jones (Jonesy for short).', 'My favorite band is Radiohead']
+    let answers = ['yes', 'y', 'no', 'n']
+    let yesResponses = ['That\'s right! I am originally from the Bay Area!', 'Hell yeah, Dubnation baby',
+                        'Yup! It\'s one of my favorite activities! Even though I can never figure out what to make lol.',
+                        'Yup wanna see pictures?', 'Ding ding ding! I\'ll trade my first born child for another album.']
+    let noResponses = ['Nope! I\'m actually originally from the Bay Area.', 'If you get to know me, you\'ll see that all I ever talk about are the Warriors.',
+                    'Let me cook something for you sometimes, you\'ll see.', 'I do so! Wanna see pictures?', 'I mean don\'t I look sad and pretenious to you?']
+
+    for(let i = 0; i < questions.length; i++) {
+    let userAnswer = prompt(questions[i])
+        if((userAnswer.toLowerCase() == answers[0]) || (userAnswer.toLowerCase() == answers [1])){
+            alert(yesResponses[i]);
+            score++
+        } else if((userAnswer.toLowerCase() == answers[2]) || (userAnswer.toLowerCase() == answers[3])){
+            alert(noResponses[i])
+        } 
     }
 
     let favNum = Math.floor(Math.random() * 10) + 1;
@@ -82,10 +54,10 @@ if(consent.toLowerCase() == 'yes' || consent.toLowerCase() == 'y'){
     }
 
     let travel = ['cuba', 'vietnam', 'japan', 'germany', 'china', 'canada', 'mexico', 'denmark']
-    let guess = 6
+    let travelGuesses = 6
     let correct = false
 
-    while(guess > 0 && !correct){
+    while(travelGuesses > 0 && !correct){
         let travelAnswer = prompt('What is a country that I have traveled to?')
         for(let i = 0; i < travel.length; i++){
             if(travelAnswer.toLowerCase() == travel[i]) {
@@ -98,9 +70,9 @@ if(consent.toLowerCase() == 'yes' || consent.toLowerCase() == 'y'){
         if(correct != true){
             alert('That\'s wrong. Let\'s try again.');
         }
-        guess--;
+        travelGuesses--;
     }
-    if(guess === 0){
+    if(travelGuesses === 0){
         alert('I\'ll just give you the answers! I\'ve been to Cuba, Denmark, Germany, Vietnam, Japan, China, Canada, and Mexico!')
     }
     //Shout out Audrey and my roommate Ahsan for helping me with this^
